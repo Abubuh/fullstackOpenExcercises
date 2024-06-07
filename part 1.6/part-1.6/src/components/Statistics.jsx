682c0b90578eb15804fb56f21fad9c4a1b13ab82
abubuh <cristian_caht@hotmail.com>
2024-06-07 11:30:45 -0500

    Add part 1.11, Statistics table

--- part 1.6/part-1.6/src/components/Statistics.jsx
+++ part 1.6/part-1.6/src/components/Statistics.jsx
@@ -3,15 +3,20 @@ import StatiscticLine from './StatiscticLine'
 
 const Statistics = ({good, neutral, bad, hasValues, total}) => {
   return (
-          <div>
+    <div>
         <h2>statistics</h2>
-        <StatiscticLine text='Good' value={good}/>
-        <StatiscticLine text='Neutral' value={neutral}/>
-        <StatiscticLine text='Bad' value={bad}/>
-        <StatiscticLine text='All' value={total}/>
-        <StatiscticLine text='Average' value={hasValues === false ? 0 : ((good - bad)/ total).toFixed(2) }/>
-        <StatiscticLine text='Positive' value={hasValues === false ?  0 : (good / (good + neutral + bad) *100).toFixed(2) + '%'}/>
-      </div>
+        <table>
+            <tbody>
+                
+                <StatiscticLine text='Good' value={good}/>
+                <StatiscticLine text='Neutral' value={neutral}/>
+                <StatiscticLine text='Bad' value={bad}/>
+                <StatiscticLine text='All' value={total}/>
+                <StatiscticLine text='Average' value={hasValues === false ? 0 : ((good - bad)/ total).toFixed(2) }/>
+                <StatiscticLine text='Positive' value={hasValues === false ?  0 : (good / (good + neutral + bad) *100).toFixed(2) + '%'}/>
+            </tbody>
+        </table>
+    </div>
   )
 }
 
