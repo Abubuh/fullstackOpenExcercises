259e4387c95dc3111dc2bcdea6cbac5406bf8f3a
abubuh <cristian_caht@hotmail.com>
2024-06-10 03:00:28 -0500

    Add part 2.2

--- part-2.1/src/components/Course.jsx
+++ part-2.1/src/components/Course.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = ({course}) => {
+    let total = 0 
+    course.parts.map((part) => {
+         total += part.exercises
+    })
     return (
     <div>
         <Header name={course.name}></Header>
         <Content parts={course.parts}></Content>
+        <Total total={total}></Total>
     </div>
   )
 }
---
+++ part-2.1/src/components/Total.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Total = ({total}) => {
+  return (
+    <div>total of {total} exercises</div>
+  )
+}
+
+export default Total
