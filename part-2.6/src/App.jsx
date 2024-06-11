40110210343d589db3eb9a05acae21b187290529
abubuh <cristian_caht@hotmail.com>
2024-06-10 23:22:44 -0500

    Part 2.6

---
+++ part-2.6/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleAdd = (event) => {
+    event.preventDefault()
+    setPersons([...persons, {name: newName}])
+  }
+  console.log(persons)
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div onChange={handleName}>
+          name: <input />
+        </div>
+        <div>
+          <button onClick={handleAdd} type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map((name) => {
+          console.log(name)
+          return (
+            <p key={name.id}>{name.name}</p>
+            )
+        })
+      }
+    </div>
+  )
+}
+
+export default App
